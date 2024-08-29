2f781433c694ee418bdf7f3e68624f1eea1b9f3a
Sulaiman <SulaimanHasan411@gmail.com>
2024-08-28 23:39:19 -0400

    add button to change grid size

--- javascript.js
+++ javascript.js
@@ -7,7 +7,27 @@ container.style.alignItems = "center";
 
 createBoard(squares);
 
+const btn = document.querySelector(".bttn");
+btn.style.display = "flex";
+btn.style.justifyContent = "center";
+btn.style.alignItems = "center";
+btn.style.margin = "20px";
+
+const gridButton = document.querySelector(".grid_button");
+
+gridButton.addEventListener("click", function() {
+    newLen = parseInt(prompt("Enter new grid size 1-100"));
+    if (!newLen || newLen < 0 || newLen > 100){
+        alert("Not a valid number, needs to be between 1-100");
+        newLen = 16;
+    }
+    createBoard(newLen);
+})
+
 function createBoard(len) {
+    while(container.hasChildNodes()){
+        container.removeChild(container.firstChild);
+    }
     for (let i = 0; i < len; i++){
         let row = document.createElement("div");
         for (let j = 0; j < len; j++){
